3fdd11a7680eab72106d6ddace865b07af8eae98
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-09-13 14:49:37 -0400

    update

--- build/alpine.sh
+++ build/alpine.sh
@@ -5,6 +5,6 @@ apk update && \
 	apk add build-base \
     libxml2-dev \
     libxslt-dev \
-    postgresql-dev \
+    postgres \
     curl unzip libexif udev chromium chromium-chromedriver wait4ports xvfb xorg-server dbus ttf-freefont mesa-dri-swrast \
     && rm -rf /var/cache/apk/*
